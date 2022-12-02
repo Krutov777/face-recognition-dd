ae98cb2dacadd0bf0ad9c8dddc347a5a47f98400
xincas <ismrah.25@gmail.com>
2022-12-02 23:18:44 +0300

    fixed model

--- model.py
+++ model.py
@@ -3,12 +3,12 @@ from facenet_pytorch import MTCNN, InceptionResnetV1
 from PIL import Image
 
 mtcnn = MTCNN(image_size=122, margin=0)
-model = InceptionResnetV1(pretrained='VGGFace2').eval()
+model = InceptionResnetV1(pretrained='vggface2').eval()
 model.classify = True
 
 def set_model(a = 0):
     if a == 0:
-        model = InceptionResnetV1(pretrained='VGGFace2').eval()
+        model = InceptionResnetV1(pretrained='vggface2').eval()
     else:
         model = InceptionResnetV1(pretrained='casia-webface').eval()
 
@@ -19,8 +19,8 @@ def compare_images(img1, img2):
     img1_croped = mtcnn(img1)
     img2_croped = mtcnn(img2)
 
-    img1_probs = model(img1_croped).detach().numpy()[0]
-    img2_probs = model(img2_croped).detach().numpy()[0]
+    img1_probs = model(img1_croped.unsqueeze(0)).detach().numpy()[0]
+    img2_probs = model(img2_croped.unsqueeze(0)).detach().numpy()[0]
     sim = cosin_metric(img1_probs, img2_probs)
 
     return sim
